b27aa0b630b35723a3e4bfdf7a62e5712a381d1a
Harsh Vishwakarma <harsh17v@gmail.com>
2026-05-29 15:28:02 +0530

    fixed db connection issue

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 async function connectDB() {
 
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGODB_URI)
 
         console.log('Connected to DB')
     } catch (error) {
